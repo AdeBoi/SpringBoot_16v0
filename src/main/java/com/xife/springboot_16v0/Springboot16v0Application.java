519c18ace6af6d7d81f23c52a89d9417f696b210
adeboi <adebotheentertainer@gmail.com>
2019-04-03 20:22:07 -0400

    Initial commit

---
+++ src/main/java/com/xife/springboot_16v0/Springboot16v0Application.java
@@ -0,0 +1,13 @@
+package com.xife.springboot_16v0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot16v0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot16v0Application.class, args);
+	}
+
+}
